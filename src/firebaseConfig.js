5f3bf3ea2aa6e1dd48654407fdfb537e3dbadf0b
Daniel Kluver <kluve018@umn.edu>
2021-03-28 19:39:23 -0500

    added a datastorage example

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,14 +4,11 @@ import 'firebase/storage'
 import "firebase/auth"
 
 const firebaseConfig = {
-        
-  };
-var firebaseConfig = {
   apiKey: process.env.VUE_APP_API_KEY,
   authDomain: process.env.VUE_APP_AUTH_DOMAIN,
   projectId: process.env.VUE_APP_PROJECT_ID,
   storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_API_KEY,
+  messagingSenderId: process.env.VUE_APP_MSG_SENDER_ID,
   appId: process.env.VUE_APP_ID
 };
 firebase.initializeApp(firebaseConfig)
